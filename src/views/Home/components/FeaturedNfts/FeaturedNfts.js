2acb9e88bfe6dd06ea2537fb9465cdbbb2871d42
Guilherme <guilhermebianeck@gmail.com>
2024-11-30 21:59:29 +0000

    Remove FeaturedNFTs

--- src/views/Home/components/FeaturedNfts/FeaturedNfts.js
+++ src/views/Home/components/FeaturedNfts/FeaturedNfts.js
@@ -1,48 +1,13 @@
-import React, { useState } from 'react'; 
-import PropTypes from 'prop-types';
-import Slider from 'react-slick';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 
-const FeaturedNfts = ({ data = [] }) => {
+const FeaturedNfts = () => {
   const theme = useTheme();
   const isMd = useMediaQuery(theme.breakpoints.up('md'), { defaultMatches: true });
-
-  const [openDialog, setOpenDialog] = useState(false);
-  const [selectedSensor, setSelectedSensor] = useState({});
-  
-  // Flag to toggle the visibility of the slider (Featured NFTs)
-  const showFeaturedNfts = false; // Change to true to display the slider
-
-  const handleSensorClick = (sensorDetails) => {
-    setSelectedSensor(sensorDetails);
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
-
-  const sliderOpts = {
-    dots: true,
-    arrows: false,
-    infinite: true,
-    slidesToShow: isMd ? 3 : 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  };
 
   return (
     <Box>
@@ -105,102 +70,40 @@ const FeaturedNfts = ({ data = [] }) => {
           </Button>
         </Box>
       </Box>
-      {/* Conditionally render the slider */}
-      {showFeaturedNfts && (
-        <Box maxWidth={{ xs: 420, sm: 620, md: 1 }} margin={'0 auto'}>
-          <Slider {...sliderOpts}>
-            {data.map((item, i) => (
-              <Box key={i} padding={{ xs: 1, md: 2, lg: 3 }}>
-                <Box
-                  display={'block'}
-                  width={1}
-                  height={1}
-                  sx={{
-                    textDecoration: 'none',
-                    transition: 'all .2s ease-in-out',
-                    '&:hover': {
-                      transform: translateY(-${theme.spacing(1 / 2)}),
-                    },
-                  }}
-                >
-                  <Box
-                    component={Card}
-                    width={1}
-                    height={1}
-                    display={'flex'}
-                    flexDirection={'column'}
-                    sx={{ backgroundImage: 'none' }}
-                  >
-                    <CardMedia
-                      title={item.name}
-                      image={item.image}
-                      sx={{
-                        position: 'relative',
-                        height: { xs: 240, sm: 340, md: 280 },
-                        overflow: 'hidden',
-                      }}
-                    />
-                    <CardContent>
-                      <Typography
-                        variant={'h6'}
-                        gutterBottom
-                        align={'left'}
-                        sx={{ fontWeight: 700 }}
-                      >
-                        {item.name}
-                      </Typography>
-                      <Box marginTop={2} display={'flex'} alignItems={'center'}>
-                        <Typography variant={'subtitle1'} color="text.secondary">
-                          {item.description}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                    <Box flexGrow={1} />
-                    <CardActions sx={{ justifyContent: 'flex-end' }}>
-                      <Button>Link para NFT</Button>
-                      {item.sensorDetails && (
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          onClick={() => handleSensorClick(item.sensorDetails)}
-                        >
-                          Detalhes do Sensor
-                        </Button>
-                      )}
-                    </CardActions>
-                  </Box>
-                </Box>
-              </Box>
-            ))}
-          </Slider>
-          <Dialog open={openDialog} onClose={handleCloseDialog}>
-            <DialogContent>
-              <DialogContentText>
-                {selectedSensor && (
-                  <>
-                    Sensor: {selectedSensor.name || 'N/A'}
-                    <br />
-                    Type: {selectedSensor.type || 'N/A'}
-                  </>
-                )}
-              </DialogContentText>
-            </DialogContent>
-            {selectedSensor && (selectedSensor.name || selectedSensor.type) && (
-              <DialogActions>
-                <Button onClick={handleCloseDialog} color="primary">
-                  Fechar
-                </Button>
-              </DialogActions>
-            )}
-          </Dialog>
-        </Box>
-      )}
+      {/* Commenting out the rest of the FeaturedNfts component */}
+      {/*
+      <Box maxWidth={{ xs: 420, sm: 620, md: 1 }} margin={'0 auto'}>
+        <Slider {...sliderOpts}>
+          {data.map((item, i) => (
+            <Box key={i} padding={{ xs: 1, md: 2, lg: 3 }}>
+              ...
+            </Box>
+          ))}
+        </Slider>
+        <Dialog open={openDialog} onClose={handleCloseDialog}>
+          <DialogContent>
+            <DialogContentText>
+              {selectedSensor && (
+                <>
+                  Sensor: {selectedSensor.name || 'N/A'}
+                  <br />
+                  Type: {selectedSensor.type || 'N/A'}
+                </>
+              )}
+            </DialogContentText>
+          </DialogContent>
+          {selectedSensor && (selectedSensor.name || selectedSensor.type) && (
+            <DialogActions>
+              <Button onClick={handleCloseDialog} color="primary">
+                Fechar
+              </Button>
+            </DialogActions>
+          )}
+        </Dialog>
+      </Box>
+      */}
     </Box>
   );
-};
-
-FeaturedNfts.propTypes = {
-  data: PropTypes.array,
 };
 
 export default FeaturedNfts;
